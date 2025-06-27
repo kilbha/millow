e44e4b9a7163da1f86077efb3ec641e643eaf190
kilbha <bhargav.mech12@gmail.com>
2025-06-27 18:08:39 +0530

    starter project - initial commit

---
+++ test/Escrow.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Escrow', () => {
+
+    it('saves the address', async() => {
+        const RealEstate = await ethers.getContractFactory('RealEstate');
+        realEstate = await RealEstate.deploy();
+
+        console.log("Address -- ", realEstate.address);
+    })
+
+})
